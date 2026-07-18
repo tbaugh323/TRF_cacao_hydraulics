cdd265efdb257b57adcc5f5fbcfa230f0b2abe04
Taylor Baugh <tbaugh@g.hmc.edu>
2026-07-18 14:41:45 -0700

    still troubleshooting

--- scripts/troubleshooting.R
+++ scripts/troubleshooting.R
@@ -1,23 +1,23 @@
 #### why am I getting negative gs? ####
 library(tidyverse)
 
-humidity <- sum_small_data |> 
+humidity <- sum_by_canopy |> 
   group_by(dt, Date, period) |> 
   summarize(rh_m = mean(rh_s_m),
             rh_sd = sd(rh_s_m)) |> 
   ungroup()
 
-smaller_measurements |> 
+clean_all_data |> 
   ggplot(aes(x = dt)) +
   geom_point(aes(y = gsw, color = factor(individual), shape = canopy), size = 2) +
-  geom_point(aes(y = rh_s / 100), size = 2, alpha = 0.5, color = "navyblue") +
+  geom_point(aes(y = rh_s / 100), size = 1, alpha = 0.5, color = "navyblue") +
   # geom_line(aes(y = rh_s / 100)) +
   # geom_errorbar(data = humidity, aes(x = dt, ymin = (rh_m - rh_sd)/100, ymax = (rh_m + rh_sd)/100), width = 1500) +
   # geom_line(data = humidity, aes(x = dt, y = rh_m / 100)) +
   scale_y_continuous(expression(paste(g[s], " (mol ", m^-2, s^-1, ")")),
                      sec.axis = sec_axis(~ . * 100, "RH (%)"))
 
-smaller_measurements |> 
+clean_all_data |> 
   ggplot(aes(x = dt)) +
   geom_point(aes(y = gsw, color = factor(individual), shape = canopy), size = 2) +
   geom_point(aes(y = Tleaf/ 17), color = "salmon", size = 2, alpha = 0.5) +
@@ -25,19 +25,21 @@ smaller_measurements |>
                      sec.axis = sec_axis(~ . * 17, expression(paste(T[leaf], " (", degree, "C)"))))
 
 
-third <- smaller_measurements |> 
+third <- clean_all_data |> 
   filter(individual == "2" | individual == "3" | individual == "4") |> 
   group_by(Date, period) |> 
   summarize(temp = mean(Tmeas),
             rh = mean(rh_s)) |> 
   ungroup()
+summary(third)
 
-second <- smaller_measurements |> 
+second <- clean_all_data |> 
   filter(individual == "1") |> 
   group_by(Date, period) |> 
   summarize(temp = mean(Tmeas),
             rh = mean(rh_s)) |> 
   ungroup()
+summary(second)
 
 # It's really humid
 
